93bff731408c1f8237799a779f488c7c2e9ccfb5
Paolo Dossetto <dossettopaolo@gmail.com>
2021-09-20 12:40:58 +0200

    Sviluppo header

--- src/App.vue
+++ src/App.vue
@@ -28,12 +28,15 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import "./style/generals";
+@import "./style/vars";
+
+h1,
+h2,
+h3 {
+  color: $primary;
 }
+
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,58 @@
 <template>
-  <div>
-    <h1>Sono l'header</h1>
-  </div>
+  <header>
+    <a href="/" class="brand">
+      <img src="@/assets/images/logo.png" alt="">
+    </a>
+
+    <nav>
+      <ul>
+        <li v-for="(link, index) in links" :key="index">
+          <a :class="(link.current) ? 'active' : null" :href="link.url">
+            {{ link.text }}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
+import * as Links from '@/assets/data/Links.js';
 export default {
   name: 'Header',
+
+  data() {
+    return {
+      links: Links.headerLinks
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  @import "../style/mixins";
+  @import "../style/vars";
 
+  header {
+    text-align: center;
+
+    ul {
+      list-style: none;
+      @include center();
+      
+      li a {
+        display: inline-block;
+        padding: 1rem;
+        text-decoration: none;
+        font-weight: 600;
+        transition: color 0.3s;
+
+        &:hover,
+        &.active {
+          color: $primary;
+        }
+      }
+    }
+  }
 </style>
