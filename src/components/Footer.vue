d1a51f62662091de71525656d24113d999726263
Paolo Dossetto <dossettopaolo@gmail.com>
2021-09-20 11:34:47 +0200

    Creazione scaffholding

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Sono il footer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
